b4be81cc23783fcbb656581ee101ea75809ee5ef
gdjonret <djonretglo@gmail.com>
2026-06-17 20:17:42 -0700

    fixes

--- .next/server/app/api/hero-images/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/hero-images/route.js")
-R.c("server/chunks/node_modules_next_8a174a68._.js")
-R.c("server/chunks/[root-of-the-server]__2510367e._.js")
-R.m("[project]/.next-internal/server/app/api/hero-images/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/hero-images/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/hero-images/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CCW854UXBixtz0rYkkSbE3WkTrnKsGAQg9uxZXS+eYI=\"\n}"
