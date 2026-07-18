ef478c5905ba090afdce065517f4716301131906
gdjonret <djonretglo@gmail.com>
2026-07-17 17:13:44 -0700

    Update BookingCalendar.tsx

--- components/BookingCalendar.tsx
+++ components/BookingCalendar.tsx
@@ -738,7 +738,7 @@ export default function BookingCalendar({
                             mode: "payment",
                             amount: 5000,
                             currency: "usd",
-                            paymentMethodCreation: "manual",
+                            capture_method: "manual",
                             appearance: {
                                 theme: "stripe",
                                 variables: {
